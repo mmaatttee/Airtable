84d68e4cd2be4e99f62b748a816fe93073b86b14
mmaatttee <32397952+mmaatttee@users.noreply.github.com>
2022-04-04 15:28:19 -0700

    Update Translate RU-EN.js
    Multiple fields supported!

--- Translate RU-EN.js
+++ Translate RU-EN.js
@@ -1,118 +1,217 @@
-//select source langauge using ISO 2 letter code
-let source = "en";
-
-// Translate API key
-let key="OURAPIKEY";  // <= change your API here
-
 // Select table
-let table = base.getTable('Family');
+let selectTable = base.getTable('Translation');
 
 // Select view
-let currentView = table.getView('Need Translation');
+let currentView = selectTable.getView('All');
 
-// Get language list from columns
-let fields = [];
-for (let field of table.fields) {
-    if (field.name == "Use of Funds Translated"){
-      fields.push(field.name);
-    }
-    
-}
-console.log(fields);
+// Get source fields
+let sourceFields = ["Use of Funds", "Funding Information", "Thank you note Text"];
+let targetFields = ["Use of Funds Translated","Funding Information Translated", "Thank you note Text - translated"]
+console.log(sourceFields);
 
 //set counter for total
 let counter = 0;
 
-//iterate over all columns/fields with all languages
-for (let field of fields){
+//get records for Name and selected Language only from All view only
+let sourceRecords = await currentView.selectRecordsAsync({fields:["Use of Funds", "Funding Information", "Thank you note Text","Use of Funds Translated","Funding Information Translated", "Thank you note Text - translated"]}); 
 
-  //get records data for Name and selected Language only
-  //let records = await table.selectRecordsAsync({fields:["Use of Funds", field]});
-  //get records from Need Translation view only
-  let records = await currentView.selectRecordsAsync({fields:["Use of Funds", "Use of Funds Translated"]});
-
-  //select records for selected language where language field is empty and 
-  //text _code(translation text) exists
-  let nonEmptyRecords = records.records.filter(record => {
-      // this below is if we want to translate from different languages
-      let target = record.getCellValue(field);
-      // this below is if we want to translate to different languages
-      let source = record.getCellValue('Use of Funds');
-      return !target && source;
+//select records for Use of Funds
+  let nonEmptyRecordsUoF = sourceRecords.records.filter(record => {
+      let source = record.getCellValue("Use of Funds");
+      let targetUoF = record.getCellValue("Use of Funds Translated");
+      // this below returns the non-Empty records in Use of Funds      
+      return !targetUoF && source;
   });
-console.log(nonEmptyRecords.length);
+console.log("Number of non-empty Use of Funds records is ", nonEmptyRecordsUoF.length);
 
-  //convert to simple array with records to translate
-  let recordsToTranslate =  [];
+let nonEmptyRecordsFI = sourceRecords.records.filter(record => {
+      let source = record.getCellValue("Funding Information");
+      let targetFI = record.getCellValue("Funding Information Translated");
+      
+      return !targetFI && source;
+  });
+console.log("Number of non-empty Financial Info records is ", nonEmptyRecordsFI.length);
 
-  for (let record of nonEmptyRecords)
+let nonEmptyRecordsTU = sourceRecords.records.filter(record => {
+      
+      let source = record.getCellValue("Thank you note Text");
+      let targetTU = record.getCellValue("Thank you note Text - translated");
+      
+      return !targetTU && source;
+  });
+console.log("Number of non-empty Thank You Note records is ", nonEmptyRecordsTU.length);
+
+  //convert to simple array with records to transliterate #1 (Use of Funds)
+  let recordsToTranslateUoF =  [];
+  let recordsToTranslateFI =  [];
+  let recordsToTranslateTU =  [];
+
+  for (let record of nonEmptyRecordsUoF)
   {
-      recordsToTranslate.push({
+      recordsToTranslateUoF.push({
         "id" : record.id,
+         "Tfield" : "Use of Funds Translated",
         "name" : record.getCellValue("Use of Funds")
       });
   }
-  let totalNumberToTranslate = recordsToTranslate.length;
-  if (totalNumberToTranslate === 0) {
-  continue;
+
+//convert to simple array with records to transliterate #2 (Funding Information)
+  for (let record of nonEmptyRecordsFI)
+  {
+      recordsToTranslateFI.push({
+        "id" : record.id,
+        "Tfield" : "Funding Information Translated",
+        "name" : record.getCellValue("Funding Information")
+      });
   }
- 
 
-  //prepare variables for http fetch request to API
+//convert to simple array with records to transliterate #3 (City)
+  for (let record of nonEmptyRecordsTU)
+  {
+      recordsToTranslateTU.push({
+        "id" : record.id,
+        "Tfield" : "Thank you note Text - translated",
+        "name" : record.getCellValue("Thank you note Text")
+      });
+  }
+
+  let recordsToTranslate = recordsToTranslateUoF.concat(recordsToTranslateFI, recordsToTranslateTU);
+  console.log("Total number of records to translate is ", recordsToTranslate.length);
+
+//prepare variables for http fetch request to API
   let q = [];
+  let key = "AIzaSyA2-tzLyh8hUozMtxGvts37n8eNIgK4Nrw";
+  let source = "uk";
   let target = "en";
-  let url=`https://translation.googleapis.com/language/translate/v2?format=text&${source}=en&key=${key}&target=${target}`;
+  let url=`https://translation.googleapis.com/language/translate/v2?format=text&source=${source}&key=${key}&target=${target}`; //pick language feature
 
-  // batch translate and update records in increments of 10
-  while (recordsToTranslate.length > 0) {
+// batch translate and update records in increments of 50 -  UoF
+  while (recordsToTranslateUoF.length > 0) {
 
     //prepare batch of 10
-    let batch = recordsToTranslate.slice(0, 10);
-    q = batch.map(recordsToTranslate => recordsToTranslate.name);
+    let batch = recordsToTranslateUoF.slice(0, 50);
+    q = batch.map(recordsToTranslateUoF => recordsToTranslateUoF.name);
+    output.inspect(batch);
 
-    // json for fetch request
-    let data = {
+// json for fetch request
+    let datax = {
         "q": q
     };
 
-    //fetch POST request with all params
     let apiResponse = await fetch(url,{
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //credentials: 'same-origin', // include, *same-origin, omit
-        //headers: {
-        //  'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        //},
-        // redirect: 'follow', // no redirects in Airtable automation scripts
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      });
+        method: 'POST',
+        mode: 'cors',
+    body: JSON.stringify(datax)
+    },
+                                );
 
+    console.log(apiResponse);
 
-    //extract translation array from the response
-    let response = await apiResponse.json();
-    response = response.data.translations;
+    let humanResponse = await apiResponse.json();
+    humanResponse = humanResponse.data.translations;
+    console.log("This is the API Translation for UoF",humanResponse);
 
     //create batch of records to update
+        let updateRecords =[];
+        for (let i = 0; i < batch.length; i++) {
+
+          updateRecords.push({
+            id: batch[i].id,
+            fields : {
+                        "Use of Funds Translated" : humanResponse[i].translatedText
+                    }
+                            });
+                                                }
+
+    //update airtable for empty records
+        await selectTable.updateRecordsAsync(updateRecords);
+        recordsToTranslateUoF = recordsToTranslateUoF.slice(50);
+  }
+
+// batch translate and update records in increments of 50 -  FI
+  while (recordsToTranslateFI.length > 0) {
+
+    //prepare batch of 10
+    let batch = recordsToTranslateFI.slice(0, 50);
+    q = batch.map(recordsToTranslateFI => recordsToTranslateFI.name);
+    output.inspect(batch);
+
+// json for fetch request
+let datax = {
+    "q": q
+};
+
+let apiResponse = await fetch(url,{
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(datax)
+    },
+);
+
+console.log(apiResponse);
+
+let humanResponse = await apiResponse.json();
+humanResponse = humanResponse.data.translations;
+console.log(humanResponse);
+
+//create batch of records to update
     let updateRecords =[];
     for (let i = 0; i < batch.length; i++) {
 
       updateRecords.push({
         id: batch[i].id,
-        fields: { 
-              [field]: response[i].translatedText
-        }
-      });
+        fields : {
+                    "Funding Information Translated" : humanResponse[i].translatedText
+                 }
+                         });
+                                            }
 
+//update airtable for empty records
+    await selectTable.updateRecordsAsync(updateRecords);
+    recordsToTranslateFI = recordsToTranslateFI.slice(50);
 
-    }
-
-    //update airtable for empty records
-    await table.updateRecordsAsync(updateRecords);
-    recordsToTranslate = recordsToTranslate.slice(10);
   }
 
- counter++; 
-}
+// batch translate and update records in increments of 50 -  TU
+  while (recordsToTranslateTU.length > 0) {
+
+    //prepare batch of 10
+    let batch = recordsToTranslateTU.slice(0, 50);
+    q = batch.map(recordsToTranslateTU => recordsToTranslateTU.name);
+    output.inspect(batch);
+
+// json for fetch request
+let datax = {
+    "q": q
+};
+
+let apiResponse = await fetch(url,{
+    method: 'POST',
+    mode: 'cors',
+    body: JSON.stringify(datax)
+    },
+);
+
+console.log(apiResponse);
+
+let humanResponse = await apiResponse.json();
+humanResponse = humanResponse.data.translations;
+console.log(humanResponse);
+
+//create batch of records to update
+    let updateRecords =[];
+    for (let i = 0; i < batch.length; i++) {
+
+      updateRecords.push({
+        id: batch[i].id,
+        fields : {
+                    "Thank you note Text - translated" : humanResponse[i].translatedText
+                 }
+                         });
+                                            }
+
+//update airtable for empty records
+    await selectTable.updateRecordsAsync(updateRecords);
+    recordsToTranslateTU = recordsToTranslateTU.slice(50);
+
+  }
