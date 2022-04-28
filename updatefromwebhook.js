8e1590d2e7f42d15111097f634444a3cefcbf523
mmaatttee <32397952+mmaatttee@users.noreply.github.com>
2022-04-27 17:19:20 -0700

    Update updatefromwebhook.js

--- updatefromwebhook.js
+++ updatefromwebhook.js
@@ -18,53 +18,22 @@ let urlItems=`https://www.mindkits.co.nz/api/v1/order_items?order_id=${orderID}`
 let apiResponse = await fetch(urlItems,{
                                             method: "get",
                                             headers: {
-                                                      'X-AC-Auth-Token': 'XXXXX',
+                                                      'X-AC-Auth-Token': '262557690032eacfad64023e3494dafb',
                                                       'Accept'         : 'application/json'
                                                      }
                                           });
   let humanResponse = await apiResponse.json();
-  console.log(humanResponse.items[0].id, humanResponse.items[0].order_id);
+  console.log('humanresponse id',humanResponse.items[0].id, 'humanresponse order id',humanResponse.items[0].order_id);
 // call API with the order_id and create a s.o. record for every [i]
 
-for (let i = 0; i < humanResponse.total_count; i++) {
-  await selectTable.createRecordsAsync([
-    {
-        fields: {
-            "Product": [{id: productDict[`${humanResponse.items[i].item_number}`]}],
-
-        },
-    },
-    {
-        fields: {
-            "Sales Order Date": humanResponse.items[i].created_at,
-        },
-    },
-    {
-        fields: {
-            "ItemQuantity": humanResponse.items[i].quantity,
-        },
-    },
-    {
-        fields: {
-            "s.o. item ID": humanResponse.items[i].id,
-        },
-    },
-    {
-        fields: {
-            "s.o. order ID": humanResponse.items[i].order_id,
-        },
-    },
-    {
-        fields: {
-            "Discount Amount": humanResponse.items[i].discount_amount,
-        },
-    },
-    {
-        fields: {
-            "ItemUnitPrice": humanResponse.items[i].price,
-        },
-    },
-]);
-  console.log(productDict[`${humanResponse.items[i].item_number}`]);
-  
+for (let i = 0; i < list; i++) {
+    await selectTable.createRecordAsync({"Product": [{id: productDict[`${humanResponse.items[i].item_number}`]}],
+                                         "s.o. order ID": humanResponse.items[i].order_id,
+                                         "Sales Order Date": humanResponse.items[i].created_at,
+                                         "ItemQuantity": humanResponse.items[i].quantity,
+                                         "s.o. item ID": humanResponse.items[i].id,
+                                         "Discount Amount": humanResponse.items[i].discount_amount,
+                                         "ItemUnitPrice": humanResponse.items[i].price});
+                                
+    console.log('created record with "s.o. item ID" =', humanResponse.items[i].id );  
 }
